382dd5ee74ca53686ea1c60376f94817dd5309b4
Arsal Ajmal <233503@students.au.edu.pk>
2025-01-08 15:11:51 +0500

    Add files via upload

---
+++ SQLQuery1.sql
@@ -0,0 +1,20 @@
+create database test; 
+create table person (
+id int ,
+city varchar
+);
+alter table person
+add email varchar(255);
+CREATE TABLE Orders (
+    OrderID int NOT NULL,
+    OrderNumber int NOT NULL,
+    PersonID int,
+    PRIMARY KEY (OrderID),
+    FOREIGN KEY (PersonID) REFERENCES Persons(PersonID)
+);
+
+INSERT INTO Customers (CustomerName, ContactName, Address, City, PostalCode, Country)
+VALUES
+('Cardinal', 'Tom B. Erichsen', 'Skagen 21', 'Stavanger', '4006', 'Norway'),
+('Greasy Burger', 'Per Olsen', 'Gateveien 15', 'Sandnes', '4306', 'Norway'),
+('Tasty Tee', 'Finn Egan', 'Streetroad 19B', 'Liverpool', 'L1 0AA', 'UK');
---
+++ finals/finals/Student.cs
@@ -0,0 +1,10 @@
+﻿// Student.cs
+public class Student
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Grade { get; set; }
+    public string Subject { get; set; }
+    public decimal Marks { get; set; }
+    public decimal Attendance { get; set; }
+}
