43896cfc0ed4aa646fb5d6e2771c4a31bfd3ac58
Arsal Ajmal <233503@students.au.edu.pk>
2025-01-08 15:26:24 +0500

    Add files via upload

---
+++ question2/finals2/Services/ProductService.cs
@@ -0,0 +1,18 @@
+﻿using System.Net.Http.Json;
+using finals2.Models;
+
+
+public class ProductService
+{
+    private readonly HttpClient _httpClient;
+
+    public ProductService(HttpClient httpClient)
+    {
+        _httpClient = httpClient;
+    }
+
+    public async Task<List<Product>> GetProductsAsync()
+    {
+        return await _httpClient.GetFromJsonAsync<List<Product>>("https://localhost:5001/api/products");
+    }
+}
